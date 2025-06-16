2d6d46affa7c83239f16558ed3dd9a578a16d63f
Dinesh2028 <dineshkumar20072002@gmail.com>
2025-06-16 22:34:35 +0530

    fix: set correct host and port for Render

--- app.py
+++ app.py
@@ -13,3 +13,10 @@ def add():
     if task:
         todos.append(task)
     return redirect('/')
+
+# ADD THIS BLOCK 👇
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
